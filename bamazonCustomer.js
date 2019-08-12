1c7557ef424f9fe6ca0293765c37e15946e8843e
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2019-08-12 11:05:47 -0700

    customer view working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ const table = new Table({
   , colWidths: [10, 30, 30, 10, 20]
 });
 
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'nodeuser',
@@ -35,43 +35,94 @@ function pickToBid(idArr) {
                 pickToBid(idArr);
             } else {
                 console.log(response.selectedItem);
-                quantity(response.selectedItem);
+                getItemDetails(response.selectedItem);
             }
         });
 }
 
-function quantity(id) {
-    connection.query('SELECT * FROM products WHERE item_id = ?', id, function (err, response) {
+function getItemDetails(id) {
+    let itemId = {item_id : parseInt(id)};
+    connection.query('SELECT * FROM products WHERE ?', itemId, function (err, response) {
         if (err) throw err;
-        console.log(response);
+        quantity(response[0].stock_quantity, itemId, response[0].price);
+    });
+}
+
+function updateDatabase(updateId, newAmt, updatePrice, bought) {
+    let updateItemId = updateId;
+    let itemPrice = parseFloat(updatePrice);
+    let numberBought = parseInt(bought);
+    console.log(updateItemId);
+    let storedNewAmt = parseInt(newAmt);
+    console.log(newAmt);
+    let total = numberBought * itemPrice;
+    let updateStock = {stock_quantity : storedNewAmt};
+    console.log(updateStock);
+    connection.query('UPDATE products SET ? WHERE ?', [updateStock, updateItemId], function (err, response) {
+        if (err) throw err;
+        console.log(`Your transaction was successful. The total was: ${total.toFixed(2)}.`);
         connection.end();
     });
+}
+
+function quantity(amt, quantId, quantPrice) {
+    let storedId = quantId;
+    let available = amt;
+    let storedPrice = quantPrice;
     inquirer
         .prompt([
             {
-                type: 'input',
+                type: 'number',
                 message: 'How many would you like to purchase?',
                 name: 'itemQuantity'
             }
         ])
-        .then(function (response) {
+        .then(function (response) {   
             console.log(response.itemQuantity);
-            if (parseInt(response.itemQuantity) > 0) {
-                console.log('working on it');
+            console.log(available);
+            if (parseInt(response.itemQuantity) > parseInt(available)) {
+                console.log('Insufficient quantity!');
+                insufficientQuestion(available);
+            } else {
+                let newAmt = parseInt(available) - parseInt(response.itemQuantity);
+                updateDatabase(storedId, newAmt, storedPrice, response.itemQuantity);
             }
         });
 }
 
-connection.query('SELECT * FROM products', function (err, response) {
-    if (err) throw err;
-    let databaseIds = [];
-    for (let i = 0; i < response.length; i++) {
-        table.push([response[i].item_id, response[i].product_name, response[i].department_name, response[i].price.toFixed(2), response[i].stock_quantity]);
-        databaseIds.push(response[i].item_id);
-    }
-    connection.end();
-    displayItems(databaseIds);
+function insufficientQuestion(availAmt) {
+    let storedAvailable = availAmt;
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'Would you like to purchase a different quantity?',
+            choices: ['yes', 'no'],
+            name: 'newItemQuantity'
+        }
+    ])
+    .then(function (response) {   
+        if (response.newItemQuantity === 'yes') {
+            quantity(storedAvailable);
+        } else {
+            return;
+        }
+    });
+}
+
+function getProducts() {
+    connection.query('SELECT * FROM products', function (err, response) {
+        if (err) throw err;
+        let databaseIds = [];
+        for (let i = 0; i < response.length; i++) {
+            table.push([response[i].item_id, response[i].product_name, response[i].department_name, response[i].price.toFixed(2), response[i].stock_quantity]);
+            databaseIds.push(response[i].item_id);
+        }
+        displayItems(databaseIds);
+    });
+}
+
+connection.connect(function(err) {
+  if (err) throw err;
+  getProducts();
 });
-
-
-
